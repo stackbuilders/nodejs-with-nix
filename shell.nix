7b4c9413bfc363eadb7b1aa99c2b6b784d643661
Sebastián Estrella <sestrella.me@gmail.com>
2019-05-16 20:57:52 -0500

    Name Nix derivation based on package.json project name

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs; stdenv.mkDerivation {
-  name = "blog-api";
+  name = (builtins.fromJSON (builtins.readFile ./package.json)).name;
   buildInputs = [
     nodePackages.node2nix
     nodejs-10_x
