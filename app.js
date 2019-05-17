e2fd6c14a7c56de930869b18ea21e2b6d1a651b6
Sebastián Estrella <sestrella.me@gmail.com>
2019-05-16 21:06:35 -0500

    Remove CSS and HTML assets

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,9 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
