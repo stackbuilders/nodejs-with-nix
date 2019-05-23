a1bf88e9d80401b0c2cdd09583f2a6f9577abf8f
Sebastián Estrella <sestrella.me@gmail.com>
2019-05-22 21:25:32 -0500

    Scaffold frontend

--- shell.nix
+++ shell.nix
@@ -1,23 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs; stdenv.mkDerivation {
-  name = (builtins.fromJSON (builtins.readFile ./package.json)).name;
+  name = "nodejs-with-nix";
   buildInputs = [
     nodePackages.node2nix
     nodejs-10_x
-    postgresql
   ];
-  shellHook = ''
-    export PGDATA="$PWD/.postgresql/data"
-
-    export DATABASE_URL="postgresql:///postgres"
-
-    if [ ! -d $PGDATA ]; then
-      initdb -D $PGDATA
-    fi
-
-    pg_ctl start -D $PGDATA -l .postgresql/log
-
-    alias node2nix="node2nix --nodejs-10"
-  '';
 }
