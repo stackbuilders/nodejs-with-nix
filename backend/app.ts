1e065ee7f5b876e2b859031e286ade59cbe9736b
Sebastián Estrella <sestrella.me@gmail.com>
2019-05-29 16:56:41 -0500

    Migrate backend code to TS

---
+++ backend/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import logger from 'morgan';
+
+var usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+
+export default app;
