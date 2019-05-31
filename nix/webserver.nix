0c22ac139b4e9fa8ffb1d8b7fc5f8de3043c1d66
Nicko Vivar D <nickovivar@gmail.com>
2019-05-31 16:49:34 -0500

    Refactor NixOps files

---
+++ nix/webserver.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+let 
+  backend = (import ../backend {}).package.override {
+    postInstall = ''
+      npm run build
+    '';
+  };
+  frontend = (import ../frontend {}).package.override {
+    postInstall = ''
+      npm run build
+    '';
+  };
+in {
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."default" = {
+      locations = {
+        "/" = {
+          root = "${frontend}/lib/node_modules/frontend/build";
+        };
+
+        "/api/" = {
+          proxyPass = "http://localhost:3000/";
+        };
+      };
+    };
+  };
+
+  systemd.services.backend = {
+    enable = true;
+    serviceConfig = {
+      WorkingDirectory = "${backend}/lib/node_modules/backend/dist";
+      ExecStart = "${pkgs.nodejs-10_x}/bin/node ./server.js";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+}
