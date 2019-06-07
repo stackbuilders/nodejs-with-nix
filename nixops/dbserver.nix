acb90fd9161cdc90f8ac4589e05a14592f1cecaa
Nicko Vivar D <nickovivar@gmail.com>
2019-06-07 14:40:35 -0500

    Add systemd service for Postgres

--- nixops/dbserver.nix
+++ nixops/dbserver.nix
@@ -3,8 +3,26 @@
 {
   services.postgresql = {
     enable = true;
+    port = 5432;
+    superUser = root;
     ensureDatabase = [
       "backend"
     ];
+  };
+
+  systemd.services.postgresql = {
+    enable = true;
+    serviceConfig = {
+      ExecStart = "/usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data";
+      ExecReload = "/bin/kill -HUP $MAINPID"; 
+      User = "postgres";
+      Group = "postgres";
+      Type = "notify";
+      KillSignal = "SIGINT";
+      KillMode = "mixed";
+      TimeoutSec = 0;   
+    };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
   };
 }
