7791bfddc46aa1039f24f843653afcf50e9fb559
Sebastián Estrella <sestrella.me@gmail.com>
2019-06-12 15:27:24 -0500

    Relocate PostgreSQL directories (relative to project location)

--- backend/shell.nix
+++ backend/shell.nix
@@ -6,12 +6,27 @@ with pkgs; mkShell {
     postgresql_11
   ];
   shellHook = ''
-    export PGDATA="$PWD/.nix-shell/var/lib/postgres/data"
+    export NIX_SHELL_DIR="$PWD/.nix-shell"
+
+    export PGDATA="$NIX_SHELL_DIR/var/lib/postgres/data"
+    export PGDATABASE="backend"
+    export PGHOST="$NIX_SHELL_DIR/tmp"
+    export PGLOG="$NIX_SHELL_DIR/var/log/postgres"
 
     if [ ! -d $PGDATA ]; then
-      ${pkgs.postgresql_11}/bin/pg_ctl init
+      ${pkgs.postgresql_11}/bin/initdb --auth=trust > /dev/null
     fi
 
-    ${pkgs.postgresql_11}/bin/pg_ctl start -o "-c listen_addresses="
+    if [ ! -d $PGLOG ]; then
+      mkdir -p $PGLOG
+    fi
+
+    if [ ! -d $PGHOST ]; then
+      mkdir -p $PGHOST
+    fi
+
+    ${pkgs.postgresql_11}/bin/pg_ctl start -l "$PGLOG/logfile" -o "-c listen_addresses= -c unix_socket_directories=$PGHOST"
+
+    trap "${pkgs.postgresql_11}/bin/pg_ctl stop" EXIT
   '';
 }
