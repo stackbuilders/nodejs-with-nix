2ad1ca27c4aed370f71c54d4437c6578b5130ead
Sebastián Estrella <sestrella.me@gmail.com>
2019-06-16 23:08:35 -0500

    Remove local PostgreSQL instance (simplify tutorial)

--- backend/shell.nix
+++ backend/shell.nix
@@ -4,41 +4,5 @@ with pkgs; mkShell {
   buildInputs = [
     nodePackages_10_x.node2nix
     nodejs-10_x
-    postgresql_11
   ];
-  shellHook = ''
-    export PGDATA="$PWD/.nix-shell/postgres/data"
-    export PGHOST="$PWD/.nix-shell/postgres/host"
-    export PGLOG="$PWD/.nix-shell/postgres/log"
-
-    export PGDATABASE="backend"
-
-    export BACKEND_DATABASE_URL="postgresql:///$PGDATABASE?host=$PGHOST"
-
-    stop() {
-      if [ -f "$PGDATA/postmaster.pid" ]; then
-        pg_ctl stop
-      fi
-    }
-
-    if [ ! -d "$PGDATA" ]; then
-      initdb --auth=trust > /dev/null
-    fi
-
-    if [ ! -d "$PGHOST" ]; then
-      mkdir -p "$PGHOST"
-    fi
-
-    if [ ! -d "$PGLOG" ]; then
-      mkdir -p "$PGLOG"
-    fi
-
-    pg_ctl start \
-      -l "$PGLOG/logfile" \
-      -o "-c listen_addresses= -c unix_socket_directories=$PGHOST"
-
-    createdb 2> /dev/null
-
-    trap stop EXIT
-  '';
 }
