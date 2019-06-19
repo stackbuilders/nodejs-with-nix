2dcdec32f956378464aa984dd1b923d0736e2768
Sebastián Estrella <sestrella.me@gmail.com>
2019-06-19 01:22:20 -0500

    Run migrations using ExecStartPre

--- nixops/webserver.nix
+++ nixops/webserver.nix
@@ -1,20 +1,14 @@
 { pkgs, ... }:
 
 let 
-  backendDatabaseUrl = "postgres://postgres@192.168.56.101/backend";
   backend = (import ../backend {}).package.override {
     postInstall = ''
-      export DATABASE_URL="${backendDatabaseUrl}"
-      export NODE_ENV="production"
-
       npm run build
-      npx sequelize db:migrate 
     '';
   };
   frontend = (import ../frontend {}).package.override {
     postInstall = ''
       export REACT_APP_CLIENT_BASE_URL="http://192.168.56.102/api"
-
       npm run build
     '';
   };
@@ -28,7 +22,6 @@ in {
         "/" = {
           root = "${frontend}/lib/node_modules/frontend/build";
         };
-
         "/api/" = {
           proxyPass = "http://localhost:3000/";
         };
@@ -39,11 +32,12 @@ in {
   systemd.services.backend = {
     enable = true;
     environment = {
-      DATABASE_URL = backendDatabaseUrl;
+      DATABASE_URL = "postgres://root@192.168.56.104/backend";
     };
     serviceConfig = {
-      WorkingDirectory = "${backend}/lib/node_modules/backend/dist";
-      ExecStart = "${pkgs.nodejs-10_x}/bin/node ./server.js";
+      WorkingDirectory = "${backend}/lib/node_modules/backend";
+      ExecStartPre = "${pkgs.nodejs-10_x}/bin/npx sequelize db:migrate";
+      ExecStart = "${pkgs.nodejs-10_x}/bin/node ./dist/server.js";
     };
     wantedBy = [ "multi-user.target" ];
   };
