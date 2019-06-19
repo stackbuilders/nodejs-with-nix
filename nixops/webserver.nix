b7a443a249366804320a468677194c0da86ac4d2
Sebastián Estrella <sestrella.me@gmail.com>
2019-06-19 16:54:58 -0500

    Remove hardcoded IPs

--- nixops/webserver.nix
+++ nixops/webserver.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let 
   backend = (import ../backend {}).package.override {
@@ -8,7 +8,7 @@ let
   };
   frontend = (import ../frontend {}).package.override {
     postInstall = ''
-      export REACT_APP_CLIENT_BASE_URL="http://192.168.56.102/api"
+      export REACT_APP_CLIENT_BASE_URL="http://${config.networking.privateIPv4}/api"
       npm run build
     '';
   };
@@ -21,9 +21,7 @@ in {
       locations = {
         "/" = {
           root = "${frontend}/lib/node_modules/frontend/build";
-          extraConfig = ''
-            try_files $uri /index.html;
-          '';
+          tryFiles = "$uri /index.html";
         };
         "/api/" = {
           proxyPass = "http://localhost:3000/";
@@ -35,11 +33,11 @@ in {
   systemd.services.backend = {
     enable = true;
     environment = {
-      DATABASE_URL = "postgres://root@192.168.56.101/backend";
+      DATABASE_URL = "postgres://root@dbserver/backend";
     };
     serviceConfig = {
       WorkingDirectory = "${backend}/lib/node_modules/backend";
-      ExecStartPre = "${pkgs.nodejs-10_x}/bin/npx sequelize db:migrate && ${pkgs.nodejs-10_x}/bin/npx sequelize db:seed:all";
+      ExecStartPre = "${pkgs.nodejs-10_x}/bin/npx sequelize db:migrate";
       ExecStart = "${pkgs.nodejs-10_x}/bin/node ./dist/server.js";
     };
     wantedBy = [ "multi-user.target" ];
--- shell.nix
+++ shell.nix
@@ -1,9 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs; mkShell {
-  buildInputs = [
-    nixops
-    nodePackages_10_x.node2nix
-    nodejs-10_x
-  ];
+  buildInputs = [ nixops ];
 }
